d9f8d7aec0a89f605dff0a9add07a8bc8551f5f3
Alireza <alireza.kariminejad@yahoo.com>
2023-01-15 12:14:31 +0330

    Paint home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_application/constants/constants.dart';
 
 class HomeWidget extends StatefulWidget {
   const HomeWidget({super.key});
@@ -11,8 +12,39 @@ class _HomeWidgetState extends State<HomeWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: lightGray,
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text(
+          'کووییز کوویین',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: classicBlue,
+        foregroundColor: yellow,
+      ),
       body: SafeArea(
-        child: Text('علیرضا کریمی نژاد'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(image: AssetImage('images/welcome.png')),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200.0, 70.0),
+                backgroundColor: green,
+                foregroundColor: Colors.white,
+                elevation: 5.0,
+              ),
+              onPressed: () {},
+              child: Text(
+                'شروع بازی',
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

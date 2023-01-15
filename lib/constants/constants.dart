046eb0c5278b80f462ff45c24edc1261cf28cab9
Alireza <alireza.kariminejad@yahoo.com>
2023-01-15 14:58:21 +0330

    Implementation list of questions

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/animation.dart';
+import 'package:flutter_quiz_application/data/question.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 final Color yellow = HexColor('#ffcc51');
@@ -6,3 +7,29 @@ final Color classicBlue = HexColor('#2654a4');
 final Color lightGray = HexColor('#ececec');
 final Color green = HexColor('#09936f');
 final Color black = HexColor('#262103');
+
+List<Question> getQuestionsList() {
+  Question firstQuestion = Question();
+  firstQuestion.questionTitle = 'مشهورترین شعبده‌باز دنیا چه کسی است؟';
+  firstQuestion.imageNumber = '1';
+  firstQuestion.correctAnswer = 1;
+  firstQuestion.answers = [
+    'علیرضا کریمی نژاد',
+    'دیوید کاپرفیلد',
+    'هری هودینی',
+    'دیوید بلین',
+  ];
+
+  Question secondQuestion = Question();
+  secondQuestion.questionTitle = 'چه کشوری ایستگاه فضایی میر را بنا کرد؟';
+  secondQuestion.imageNumber = '2';
+  secondQuestion.correctAnswer = 1;
+  secondQuestion.answers = [
+    '(شوروی سابق)روسیه',
+    'آمریکا',
+    'چین',
+    'هند',
+  ];
+
+  return [firstQuestion, secondQuestion];
+}

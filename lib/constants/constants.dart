3ef7e4a88b7b7de07b9e41ecf9b4c4c5595079d7
Alireza <alireza.kariminejad@yahoo.com>
2023-01-15 12:02:14 +0330

    Add constants file

---
+++ lib/constants/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/animation.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color yellow = HexColor('#ffcc51');
+final Color classicBlue = HexColor('#2654a4');
+final Color lightGray = HexColor('#ececec');
+final Color green = HexColor('#09936f');
+final Color black = HexColor('#262103');

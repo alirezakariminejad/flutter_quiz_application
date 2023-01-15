0fa8637584d8a94dacfd9b5953510458155879a6
Alireza <alireza.kariminejad@yahoo.com>
2023-01-15 11:09:54 +0330

    Create quiz application

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
